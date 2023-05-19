158f5ef633b2e1c7fa8a566247a890ae2dbbc873
Xtrem <arturovergara@protonmail.com>
2023-05-19 11:15:06 -0400

    feat(panel): Finish custom panel styles and urls

--- apps/panel/urls.py
+++ apps/panel/urls.py
@@ -1,9 +1,10 @@
 # Django Imports
 from django.urls import path
 
-from .views import AppsPanelView
+from .views import AccessDetailView, AccessPanelView
 
 app_name = "panel"
 urlpatterns = [
-    path("", AppsPanelView.as_view(), name="home"),
+    path("access/", AccessDetailView.as_view(), name="access_detail"),
+    path("", AccessPanelView.as_view(), name="home"),
 ]
--- apps/panel/views.py
+++ apps/panel/views.py
@@ -2,5 +2,9 @@
 from django.views.generic import TemplateView
 
 
-class AppsPanelView(TemplateView):
+class AccessPanelView(TemplateView):
     template_name = "panel/index.html"
+
+
+class AccessDetailView(TemplateView):
+    template_name = "panel/app.html"
